171ee333f65deab6ee1119e6de74f2b77bd9e3af
Peter Sampson <petersampson010@gmail.com>
2019-05-16 19:48:25 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,26 @@
 //define functions here
+$(`p`).on("click", function getIt() {
+  return `Hey!`
+})
 
+$(`.tasty`).on(`load`, frameIt() {
+  return .tasty(border: 1px solid red)
+})
+
+$(document).on(`keydown`,pressIt(key) {
+  if (key.which == 71){
+    alert(`G was pressed`)
+  }
+}
+
+$("form").on("submit", function() {
+  if ($( "input:first" ).val() === "correct" {
+    alert(`your form is going to be submitted now`)
+    return
+  }
+  alert("you entered the wrong value")
+  return
+})
 $(document).ready(function(){
 
 // call functions here
